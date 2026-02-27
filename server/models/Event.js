ac9ca623f510ec624f1d140e8816e5aaf519a396
Wakjira-Tesama <wakjiratesama@gmail.com>
2026-02-27 14:15:00 +0300

    feat: create event model

---
+++ server/models/Event.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const EventSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  date: { type: Date, required: true },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Event", EventSchema);
